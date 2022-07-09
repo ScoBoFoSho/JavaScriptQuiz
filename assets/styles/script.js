657b780ce3c9f7574205e91d1167b65ab1e6eb39
ScoBoFoSho <ferrecisco@icloud.com>
2022-07-09 18:17:24 -0400

    text change

--- assets/styles/script.js
+++ assets/styles/script.js
@@ -1,2 +1,21 @@
 var timerEle = document.getElementByID("countdown");
 var mainEle = doucment.getElementByID("main");
+
+function countdown() {
+  var timeLeft = 60;
+
+  // describe function of the timer
+  var timeInterval = setInterval(function () {
+    if (timeLeft > 1) {
+      timerEle.textContent = timeLeft + "seconds remaining";
+      timeLeft--;
+    } else if (timeLeft === 1) {
+      timerEle.textContent = timeLeft + "seconds remaining";
+      timeLeft--;
+    } else {
+      timerEle.textContent = "";
+      clearInterval(timeInterval);
+      displayMessage();
+    }
+  }, 1000);
+}
